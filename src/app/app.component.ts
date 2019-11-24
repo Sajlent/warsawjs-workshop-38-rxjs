86593e4866f5d91abfeda683ba62b139eb67bb91
Sajlent <sajlent4@gmail.com>
2019-11-24 13:45:24 +0100

    Filtering & transformations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { interval, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {BehaviorSubject, fromEvent, interval, merge, of} from 'rxjs';
+import {filter, map, take, takeUntil, takeWhile, throttleTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +12,8 @@ export class AppComponent {
 
   constructor() {
     // this.observableAndObserver();
-    this.observables();
-    // this.subjects();
+    // this.observables();
+    this.subjects();
     // this.operatorsFiltering();
     // this.operatorsTransformation();
     // this.operatorsCombination();
@@ -37,25 +37,67 @@ export class AppComponent {
   observables() {
     interval(1000)
       .pipe(
-        map((val) => 1000 + val)
-      )
-      .subscribe(
-        console.log,
-        null,
-        () => { console.log('complete'); }
+        map((val) => 1000 + val),
+        filter((val) => val % 2 === 0)
       );
+      // .subscribe(
+      //   console.log,
+      //   null,
+      //   () => { console.log('complete'); }
+      // );
+
+    const mouseEv$ = fromEvent(document, 'mousemove')
+      .pipe(
+        map(({clientX, clientY}: MouseEvent) => ({ clientX, clientY })),
+        throttleTime(100)
+      );
+
+    mouseEv$.subscribe(console.log);
   }
 
   subjects() {
+    const filters = new BehaviorSubject({
+      currentPage: 1,
+      itemsPerPage: 5
+    });
+
+    filters.subscribe(console.log);
+
+    fromEvent(document, 'click').subscribe(() => {
+      filters.next({ ...filters.value, itemsPerPage: 10 });
+    });
   }
 
   operatorsFiltering() {
+    interval(1000)
+      .pipe(
+        take(25),
+        takeWhile((val) => val < 5),
+        takeUntil(fromEvent(document, 'click'))
+      )
+      .subscribe(console.log);
   }
 
   operatorsTransformation() {
   }
 
   operatorsCombination() {
+    const mouseEv$ = fromEvent(document, 'mousemove')
+      .pipe(
+        map(({ clientX, clientY }: MouseEvent) => ({ clientX, clientY })),
+        throttleTime(100)
+      );
+
+    const clickEv$ = fromEvent(document, 'click')
+      .pipe(
+        map(({ type }: MouseEvent) => ({ type }))
+      );
+
+    merge(
+      mouseEv$,
+      clickEv$
+    )
+      .subscribe(console.log);
   }
 
   hotvscold() {
