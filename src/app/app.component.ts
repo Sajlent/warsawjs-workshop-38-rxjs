14223b06cb2e85e1a312ffe8753c71d3f073663a
Sajlent <sajlent4@gmail.com>
2019-11-24 11:45:13 +0100

    Simple Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'code';
+
+  constructor() {
+    // this.observableAndObserver();
+    this.observables();
+    // this.subjects();
+    // this.operatorsFiltering();
+    // this.operatorsTransformation();
+    // this.operatorsCombination();
+    // this.hotvscold();
+    // this.higherOrder();
+    // this.customOperator();
+
+  }
+
+  observableAndObserver() {
+    of('hello')
+      .subscribe(
+        console.log,
+        console.error,
+        () => {
+          console.log('complete');
+        }
+      );
+  }
+
+  observables() {
+    interval(1000)
+      .pipe(
+        map((val) => 1000 + val)
+      )
+      .subscribe(
+        console.log,
+        null,
+        () => { console.log('complete'); }
+      );
+  }
+
+  subjects() {
+  }
+
+  operatorsFiltering() {
+  }
+
+  operatorsTransformation() {
+  }
+
+  operatorsCombination() {
+  }
+
+  hotvscold() {
+  }
+
+  higherOrder() {
+  }
+
+  customOperator() {
+  }
 }
